73d87744795d8d8d67fd7b9f907a18af89a2c25c
EunjiLee <eunji.lee@uzh.ch>
2026-04-30 21:51:48 +0200

    add FastAPI inference service

---
+++ src/api/app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import joblib
+import pandas as pd
+
+
+app = FastAPI()
+
+model = joblib.load("models/lgbm_model.pkl")
+
+# health check
+@app.get("/")
+def home():
+    return {"message": "Energy Demand Forecast API is running."}
+
+@app.post("/predict")
+def predict(data: dict):
+    df = pd.DataFrame([data])
+    pred = model.predict(df)[0]
+
+    return {
+        "prediction": float(pred)
+    }
